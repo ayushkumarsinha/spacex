b020c4c4ca64150019076ab75b568c3d033c0c3b
ayushkumarsinha <ayushkumarsinha@hotmail.com>
2021-01-20 11:32:01 +0530

    initial

---
+++ src/components/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Home(props){
+    return (
+        <>
+            <div className="col-md-6"> Ayush</div>
+            <div className="col-md-3 col-sm-12"></div>
+            <div className="col-md-9 col-sm-12">
+                <div className="card card-width">
+                    <img className="card-img-top" src="..." alt="Card image cap"/>
+                    <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+export default Home;
